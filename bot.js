18f8f56c47818075a7700e8d8f09b90f45b98bd5
Glitch (brave-paw) <none>
2020-01-08 14:54:25 +0000

    🍠👟 Checkpoint ./bot.js:8801804/561 ./komutlar/güvenlik.js:8801804/2277

--- bot.js
+++ bot.js
@@ -251,3 +251,16 @@ if(deletedMessage.author.bot || deletedMessage.channel.type === "dm") return;
 });
    
 //-------------------GÜVENLİK
+client.on('guildMemberAdd',async member => {
+  let user = client.users.get(member.id);
+  let chan = client.channels.get(db.fetch(`guvenlik${member.guild.id}`)) 
+require("moment-duration-format");
+
+    const kurulus = new Date().getTime() - user.createdAt.getTime();
+    const gün = moment.duration(kurulus).format("D")   
+    var kontrol;
+    if (kurulus < 7) kontrol = 'Güvenilir Değil!'
+    if (kurulus > 7) kontrol = 'Güvenilir!'
+
+  member.guild.channels.get('`'+ member.user.tag +'` sunucuya katıldı! Güvenilir mi? **'+ kontrol +'**')
+});
