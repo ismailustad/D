ac7a5ef74e56f7eddf77df43a57b789f105deae6
Glitch (brave-paw) <none>
2020-01-08 14:18:32 +0000

    📹👯 Checkpoint ./bot.js:8801804/1491 ./komutlar/güvenlik.js:8801804/1159

--- bot.js
+++ bot.js
@@ -249,3 +249,38 @@ if(deletedMessage.author.bot || deletedMessage.channel.type === "dm") return;
   sChannelanan.send(embed)
 });
    
+//-------------------GÜVENLİK
+client.on('guildMemberAdd',async member => {
+  let user = client.users.get(member.id);
+  let chan = client.channels.get(db.fetch(`guvenlik${member.guild.id}`)) 
+       const Canvas = require('canvas')
+       const canvas = Canvas.createCanvas(360,100);
+       const ctx = canvas.getContext('2d');
+  
+  const resim1 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614151181752860672/yhosgeldirrn.png')
+    const resim2 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164419768877056/yhosgeldirrn.png')
+    const kurulus = new Date().getTime() - user.createdAt.getTime();
+    const gün = moment.duration(kurulus).format("D")   
+    var kontrol;
+      if (kurulus > 2629800000) kontrol = resim2
+    if (kurulus < 2629800000) kontrol = resim1
+
+       const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164413318168606/Adsz.png');
+       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+   
+
+
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+  ctx.drawImage(kontrol,0,0,canvas.width, canvas.height)
+  ctx.beginPath();
+    ctx.lineWidth = 4;
+  ctx.fill()
+    ctx.lineWidth = 4;
+  ctx.arc(180, 46, 36, 0, 2 * Math.PI);
+    ctx.clip();
+  ctx.drawImage(avatar, 143,10, 73, 72  );
+
+   
+       const attachment = new Discord.Attachment(canvas.toBuffer(), 'STARKs-güvenlik.png');
+    chan.send(attachment)
+});
